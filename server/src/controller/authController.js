161586dfa089b3b37308b4b53dc8a5ed73558be6
hieutts <hieutt03.work@gmail.com>
2024-03-16 12:15:02 +0700

    conflict

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -280,9 +280,9 @@ class AuthController {
                 message: "Register successfully.",
                 access_token: access_token,
                 data: {
-                    // user: {
+                    user: {
                         email
-                    // }
+                    }
                 }
             });
         } catch (error) {

45caacfd08d4a4b1f74292376060e081efe9ec60
hieutts <hieutt03.work@gmail.com>
2024-04-12 16:33:56 +0700

    change host db

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,5 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "http://54.159.236.245:5173/"
 })
--- server/src/database/connectDbPg.js
+++ server/src/database/connectDbPg.js
@@ -6,9 +6,9 @@ require("dotenv").config();
 const sequelize = new Sequelize(
     process.env.DB_NAME || 'booking_web',
     process.env.DB_USER_PG || 'postgres',
-    process.env.DB_PASSWORD || '123456',
+    process.env.DB_PASSWORD || '12345678',
     {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || 'database-1.cnw6w4ueoxn0.us-east-1.rds.amazonaws.com',
         port: process.env.DB_PORT || '5432',
         dialect: "postgres",
         pool: {
